fee817b2c39588d4bb83c32c341b0aa77024f1bb
deathart <deathart@hotmail.fr>
2016-09-18 20:59:48 +0200

    Update exemple

--- test.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var Trans = require('./translate.js');
-var Translate = new Trans("fr");
-
-var teste = Translate.GetLine("test_replace", "deathart");
-
-console.log("Value for 'username' : " + teste);
