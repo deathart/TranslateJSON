e2f5a0aa9761b76075508f0ad73f3c57cefc3081
deathart <deathart@hotmail.fr>
2016-09-18 21:01:25 +0200

    ADD readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# TranslateJSON
+
+Usage : 
+
+var Trans = require('./translate.js');
+var Translate = new Trans("fr");
+
+Translate.GetLine("test");
--- exemple.js
+++ exemple.js
@@ -3,6 +3,6 @@
 var Trans = require('./translate.js');
 var Translate = new Trans("fr");
 
-console.log("Value for 'test_replace' : " + Translate.GetLine("test_replace", "deathart"))
-console.log("Value for 'test' : " + Translate.GetLine("test"))
+console.log("Value for 'test_replace' : " + Translate.GetLine("test_replace", "deathart"));
+console.log("Value for 'test' : " + Translate.GetLine("test"));
 console.log("Value for 'username' : " + Translate.GetLine("username"));
