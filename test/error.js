70d63598677c37d2464b0ac94a73bebc65d2940b
Deathart <deathart@hotmail.fr>
2018-02-12 15:14:08 +0100

    Typo in test

--- test/error.js
+++ test/error.js
@@ -5,32 +5,33 @@ const Translate = new translatejson("en", {
 	Directory: "./test/locales/",
 })
 
-describe("GetError", () => {
-	describe("Normal", () => {
-		it("notreplace", (done) => {
-			expect(Translate.GetLine("noexists")).to.be.false
-			done()
-		})
-		it("replace", (done) => {
-			expect(Translate.GetLine("noexists", "error")).to.be.false
-			done()
-		})
-	})
-	describe("Block", () => {
-		it("notreplace", (done) => {
-			expect(Translate.GetBlock("c_block.block_test_500")).to.be.false
-			done()
-		})
-		it("replace", (done) => {
-			expect(Translate.GetBlock("d_block.block_test_a", "deathart")).to.be.false
-			done()
-		})
-	})
-})
-
-describe("DeleteError", () => {
-	it("error", (done) => {
-		expect(Translate.Del("test2")).to.be.false
-		done()
-	})
-})
+describe("Error", () => {
+    describe("Get", () => {
+        describe("Normal", () => {
+            it("notreplace", (done) => {
+                expect(Translate.GetLine("noexists")).to.be.false
+                done()
+            })
+            it("replace", (done) => {
+                expect(Translate.GetLine("noexists", "error")).to.be.false
+                done()
+            })
+        })
+        describe("Block", () => {
+            it("notreplace", (done) => {
+                expect(Translate.GetBlock("c_block.block_test_500")).to.be.false
+                done()
+            })
+            it("replace", (done) => {
+                expect(Translate.GetBlock("d_block.block_test_a", "deathart")).to.be.false
+                done()
+            })
+        })
+    })
+    describe("Delete", () => {
+        it("error", (done) => {
+            expect(Translate.Del("test2")).to.be.false
+            done()
+        })
+    })
+});
