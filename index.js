6bda9ad9a94f85614d07b4ebed3b8a576969750c
deathart <deathart@hotmail.fr>
2018-02-17 16:43:23 +0100

    Typo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs")
 
 module.exports = class Translate {
 
-    constructor(files, Directory = "./locales") {
+	constructor(files, Directory = "./locales") {
 		this.file = files
 		this.error = false
 		this.location = Directory
@@ -14,9 +14,9 @@ module.exports = class Translate {
 		} catch (err) {
 			this.error = true
 			if (err.code === "ENOENT") {
-				throw "[ERROR] : The translation of the '" + files+ "' file is not found !";
+				throw "[ERROR] : The translation of the '" + files+ "' file is not found !"
 			} else {
-				throw "[ERROR] : " + err;
+				throw "[ERROR] : " + err
 			}
 		}
 	}
@@ -24,15 +24,15 @@ module.exports = class Translate {
 	GetLine(Lines, replace = null) {
 		if (this.error === false) {
 		    if(this.resolve(Lines)) {
-                if (replace !== null) {
-                    return this.resolve(Lines).replace("%s", replace)
-                } else {
-                    return this.resolve(Lines)
-                }
-            }
-            else {
+				if (replace !== null) {
+					return this.resolve(Lines).replace("%s", replace)
+				} else {
+					return this.resolve(Lines)
+				}
+			}
+			else {
 		        return false
-            }
+			}
 		}
 	}
 
@@ -52,24 +52,24 @@ module.exports = class Translate {
 
 	SetLine(key, value) {
 		if (this.error === false) {
-            if (this.resolve(key) !== false) {
-                this.data[key] = value
-                try {
-                    fs.writeFileSync(this.loc, JSON.stringify(this.data, null, 2))
-                    return true
-                } catch (err) {
-                    return false
-                }
-            }
-            return false
+			if (this.resolve(key) !== false) {
+				this.data[key] = value
+				try {
+					fs.writeFileSync(this.loc, JSON.stringify(this.data, null, 2))
+					return true
+				} catch (err) {
+					return false
+				}
+			}
+			return false
 		}
 	}
 
 	Update(key, value) {
 		if (this.error === false) {
-            let res = this.resolve(key);
+			let res = this.resolve(key)
 			if (res) {
-                res = value
+				res = value
 				try {
 					fs.writeFileSync(this.loc, JSON.stringify(this.data, null, 2))
 					return true
@@ -99,18 +99,18 @@ module.exports = class Translate {
 		}
 	}
 
-    resolve(key) {
-        if (key.indexOf('.') > -1) {
-            return key.split('.').reduce(function (prev, curr) {
-                return prev ? prev[curr] : false
-            }, this.data)
-        }
-        else {
-            if (typeof this.data[key] !== "undefined") {
-                return this.data[key]
-            } else {
-                return false
-            }
-        }
-    }
+	resolve(key) {
+		if (key.indexOf(".") > -1) {
+			return key.split(".").reduce(function (prev, curr) {
+				return prev ? prev[curr] : false
+			}, this.data)
+		}
+		else {
+			if (typeof this.data[key] !== "undefined") {
+				return this.data[key]
+			} else {
+				return false
+			}
+		}
+	}
 }
