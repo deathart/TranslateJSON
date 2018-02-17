282a743392ccb0e3de1a1c7543d2e674297782bb
deathart <deathart@hotmail.fr>
2018-02-17 16:42:21 +0100

    Typo in test

--- test/success.js
+++ test/success.js
@@ -1,28 +1,28 @@
 const expect = require("chai").expect
 const translatejson = require("../index")
 
-const Translate = new translatejson("en", "./test/locales/")
+const Translate = new translatejson("fr", "./test/locales/")
 
 describe("Success", () => {
     describe("Get", () => {
         describe("Normal", () => {
             it("NotReplace", (done) => {
-                expect(Translate.GetLine("test")).to.equal("Hello")
+                expect(Translate.GetLine("hello")).to.equal("Bonjour")
                 done()
             })
             it("Replace", (done) => {
-                expect(Translate.GetLine("test_replace", "deathart")).to.equal("Hello deathart")
+                expect(Translate.GetLine("hello users", "deathart")).to.equal("Bonjour deathart")
                 done()
             })
         })
 
         describe("Block", () => {
             it("NotReplace", (done) => {
-                expect(Translate.GetBlock("block_test.first_block")).to.equal("[EN]block")
+                expect(Translate.GetBlock("block.first_block")).to.equal("[FR] block")
                 done()
             })
             it("Replace", (done) => {
-                expect(Translate.GetBlock("block_test.deux_block", "deathart")).to.equal("[EN]block deathart test")
+                expect(Translate.GetBlock("block.sec_block", "deathart")).to.equal("[FR] block deathart remplacé")
                 done()
             })
         })
@@ -30,25 +30,25 @@ describe("Success", () => {
 
     describe("Update", () => {
         it("Update", (done) => {
-            expect(Translate.Update("test", "Hello")).to.be.true
+            expect(Translate.Update("hello", "Hello")).to.be.true
             done()
         })
     })
 
     describe("Remove", () => {
         it("Remove", (done) => {
-            expect(Translate.Del("test")).to.be.true
+            expect(Translate.Del("hello")).to.be.true
             done()
         })
     })
 
     describe("Set", () => {
         before(() => {
-            Translate.Del("test");
+            Translate.Del("hello");
         })
 
         it("Set", (done) => {
-            expect(Translate.SetLine("test", "Hello")).to.be.true
+            expect(Translate.SetLine("hello", "Bonjour")).to.be.true
             done()
         })
     })
